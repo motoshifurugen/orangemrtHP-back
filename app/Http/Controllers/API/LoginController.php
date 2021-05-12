1fdddcde89a2ef4160cf55844f1ce1af75cfd88a
motoshifurugen <amanogawa_m@icloud.com>
2021-05-13 01:15:51 +0900

    ログインメソッド追加

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -4,22 +4,22 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\User;
 
-class LetterController extends Controller
+class LoginController extends Controller
 {
-    public function index(Request $request) {
-        // $user = DB::table('users')
-        // ->where('name', '=', $request->input('name'))
-        // ->where('email', '=', $request->input('email'))
-        // ->get();
-        // if (!empty($user)) {
-        //     $pass = $user->password;
-        //     if ($pass == $request->input('password')) {
-        //         return 'login!';
-        //     }
-        // }
+    public function login(Request $request) {
+        $user = User::where('name', '=', $request->input('name'))
+        ->where('email', '=', $request->input('email'))
+        ->first();
+        if (!empty($user)) {
+            $pass = $user["password"];
+            if ($pass == $request->input('password')) {
+                return 'login!';
+            }
+        }
         return $request;
     }
 }
